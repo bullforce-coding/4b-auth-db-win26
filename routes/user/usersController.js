69f8b90d27f4e5d1b4675519b1bae214e0d31846
glf30 <greyson.frazier@gmail.com>
2026-01-12 14:05:34 -0500

    Add login functionality

--- routes/user/usersController.js
+++ routes/user/usersController.js
@@ -2,31 +2,63 @@ const User = require("./usersModel");
 const bcrypt = require("bcrypt");
 
 const createUser = async (userData) => {
-    try {
-    
-        // 1. generate the salt
-        const salt = await bcrypt.genSalt();
+  try {
+    // 1. generate the salt
+    const salt = await bcrypt.genSalt();
 
-        // 2. encrypt the password
-        // pass in the password from the request body, along with the salt
-        const hashedPassword = await bcrypt.hash(userData.password, salt);
+    // 2. encrypt the password
+    // pass in the password from the request body, along with the salt
+    const hashedPassword = await bcrypt.hash(userData.password, salt);
 
-        // 3. create secure user object
-        // store the username along with the encrypted password inside a new object
-        const secureUserData = {
-            username: userData.username,
-            password: hashedPassword
-        }
+    // 3. create secure user object
+    // store the username along with the encrypted password inside a new object
+    const secureUserData = {
+      username: userData.username,
+      password: hashedPassword,
+    };
 
-        const newUser = await User.create(secureUserData);
+    const newUser = await User.create(secureUserData);
 
-        // prevent password from being returned
-        const returnedUser = await User.findOne({ username: userData.username }).select("-password")
+    // prevent password from being returned
+    const returnedUser = await User.findOne({
+      username: userData.username,
+    }).select("-password");
 
-        return returnedUser;
-    } catch (error) {
-        throw error;
+    return returnedUser;
+  } catch (error) {
+    throw error;
+  }
+};
+
+const loginUser = async (userData) => {
+  try {
+    // verify username exists/matches the one in db
+
+    const user = await User.findOne({ 
+        username: userData.username 
+    });
+
+    // if we don't find the user, throw error
+    if(!user){
+        throw "User not found"
     }
-}
 
-module.exports = { createUser }
+    // compare incoming password to the one in the DB
+    // incoming: userData.password
+    // db: user.password
+    const isCorrectPassword = await bcrypt.compare(userData.password, user.password);
+
+    // if passwords DONT match, throw error
+    if(!isCorrectPassword){
+        throw "Incorrect password"
+    }
+
+    // if they do, return true!
+    return user;
+
+  } catch (error) {
+    throw error;
+  }
+};
+
+module.exports = { createUser, loginUser };
--- routes/user/usersRouter.js
+++ routes/user/usersRouter.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { createUser } = require("./usersController");
+const { createUser, loginUser } = require("./usersController");
 const router = express.Router();
 
 router.post("/", async (req,res) => {
@@ -16,5 +16,21 @@ router.post("/", async (req,res) => {
         })
     }
 })
+
+router.post("/login", async (req,res) => {
+    try {
+        const userLoggedIn = await loginUser(req.body);
+        res.json({
+            message: "success",
+            payload: `${userLoggedIn.username} Successfully logged in`
+        })       
+    } catch (error) {
+        res.status(500).json({
+            message: "failure",
+            payload: error
+        })
+    }
+})
+
 
 module.exports = router;
